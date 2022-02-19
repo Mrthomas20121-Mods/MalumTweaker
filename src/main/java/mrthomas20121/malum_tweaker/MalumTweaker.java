5a7e92e93d68cc4e40ec13c4ff2d06b314b2cd32
Mrthomas20121 <thomas77300@gmail.com>
2022-02-19 14:02:50 +0100

    Main class

---
+++ src/main/java/mrthomas20121/malum_tweaker/MalumTweaker.java
@@ -0,0 +1,13 @@
+package mrthomas20121.malum_tweaker;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MalumTweaker.mod_id)
+public class MalumTweaker {
+
+	public static final String mod_id = "malum_tweaker";
+
+	public MalumTweaker() {
+
+	}
+}
